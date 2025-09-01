a52a4fa96859bbda4fa87d2e103ea51bbb49fe85
kadirdolek <kadirdolek@outlook.com>
2025-09-01 14:39:05 +0200

    modif

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::middleware(['auth', 'can:is-staff'])->group(function () {
 Route::get('/joueurs', [JoueurController::class, 'index'])->name('joueurs.index');
 
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
